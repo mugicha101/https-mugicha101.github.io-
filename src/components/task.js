5f981333069a589f7f4396f50f5530e5e28d3064
mugicha101 <alexander.yoshida@gmail.com>
2021-10-14 20:47:21 -0400

    a

--- src/components/task.js
+++ src/components/task.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Task = ({ name }) => {
+const Task = ({ name}) => {
   return (
     <div>
       {/**
        * TODO: add an html or bootstrap element that uses the name variable to display an individual task
        */}
+       <div id={"task-"+name}>
+          <li>{name}</li>
+          <button id="remove" onClick={function() {
+           document.getElementById("task-"+name).remove();
+         }}>Remove</button>
+       </div>
     </div>
   );
 };
